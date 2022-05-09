0b67cd399146eb7ad604ac9544893b68894b218f
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-08 19:16:07 -0500

    first committt

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get("/",(req, res) =>{
 })
 
 app.get('/astronomy', (req, res) => {
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
+    const page = parseInt(1)
+    const limit = parseInt(10)
 
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
